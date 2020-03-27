a32b092ede81c8f60fbcefda345f2fbd7e7e95b1
neilaldrei <neilaldreiiv@gmail.com>
2020-03-27 09:19:04 +0800

    App.vue modifications (Scrollbar, Indents and router view

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app" class="uk-position-relative">
+        <router-view />
     </div>
-    <router-view />
-  </div>
 </template>
 
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
 <style lang="less">
+body {
+    font-family: 'Open Sans', sans-serif;
+    height: 100%;
+}
+
+/* width */
+::-webkit-scrollbar {
+    width: 7px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    background: #FFCDD2;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #F44336;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #E53935;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
