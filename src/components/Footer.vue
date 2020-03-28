30257eaec25b8afcbb0f9220406bfef399515b64
neilaldrei <neilaldreiiv@gmail.com>
2020-03-28 20:17:35 +0800

    Added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <portfolio-contacts></portfolio-contacts>
+        <hr>
+        <h5><strong>{{ new Date().getFullYear() }} — Neil Aldrei</strong></h5>
+    </div>
+</template>
+
+<script>
+import Contacts from "./Contacts";
+
+export default {
+    name: "Footer",
+    components: {
+        "portfolio-contacts": Contacts,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
         <portfolio-skills></portfolio-skills>
         <portfolio-timeline></portfolio-timeline>
         <portfolio-portfolio></portfolio-portfolio>
+        <portfolio-footer></portfolio-footer>
     </div>
 </template>
 
@@ -12,6 +13,7 @@ import Skills from "@/components/Skills";
 import Banner from "@/components/Banner";
 import Timeline from "@/components/Timeline";
 import Portfolio from "@/components/Portfolio";
+import Footer from "@/components/Footer";
 
 export default {
     name: "Home",
@@ -20,6 +22,7 @@ export default {
         "portfolio-skills": Skills,
         "portfolio-timeline": Timeline,
         "portfolio-portfolio": Portfolio,
+        "portfolio-footer": Footer,
     }
 };
 </script>
